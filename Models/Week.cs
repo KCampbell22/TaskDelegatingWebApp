b0530018c049b95d76a4d6d7a140ee27a6c80762
KCampbell22 <ksoup12@gmail.com>
2022-11-24 21:39:31 -0600

    - Add week to db

--- Models/Week.cs
+++ Models/Week.cs
@@ -3,7 +3,7 @@
     public class Week
     {
         public DateOnly WeekStart { get; set; }
-        public DateOnly WeekEnd { get; set;
-        }
+        public DateOnly WeekEnd { get; set; }
+        public ICollection<Day> Days { get; set; }
     }
 }
