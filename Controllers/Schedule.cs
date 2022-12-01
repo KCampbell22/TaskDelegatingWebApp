cb55baed960d905197fb7136bb17f55400881d69
Kade Campbell <kcampbell18@dmacc.edu>
2022-12-01 10:58:12 -0600

    -ajax test

---
+++ Controllers/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskDelegatingWebApp.Controllers
+{
+    public class Schedule : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
